e84e79d4ff3be2b4b2599c14882c82592f1dee25
lixiaoqiang <lixiaoqiang@yixia.com>
2017-11-12 14:02:06 +0800

    初始百思不得姐项目

---
+++ baisibudeijie/baisibudeijie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baisibudeijie
+//
+//  Created by lixiaoqiang on 2017/11/12.
+//  Copyright © 2017年 lixiaoqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baisibudeijie/baisibudeijie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baisibudeijie
+//
+//  Created by lixiaoqiang on 2017/11/12.
+//  Copyright © 2017年 lixiaoqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
